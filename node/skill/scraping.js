52f58b3e6eca4c502945c2948dde95c9d8ada3ab
Hisashi Itoh <it.hisas@gmail.com>
2018-07-01 22:31:54 +0900

    アニメ系のキーワードによる記事のフィルタ,NGワードによる記事の除外の実装を実施

--- node/skill/scraping.js
+++ node/skill/scraping.js
@@ -7,6 +7,8 @@
 //      関数の帰り値は
 //      [日付, イベントタイトル, 詳細情報]
 //      $ npm install --save cheerio-httpcli
+//      $ npm install --save moji
+//        https://www.npmjs.com/package/moji
 //
 //      只今サポートしているWEBは
 //      - 阿佐ヶ谷ロフト:   
@@ -18,18 +20,34 @@
 //         2018.06.30 新規作成
 //
 var client = require('cheerio-httpcli');
+var moji = require('moji');
 client.set('browser','chrome');
 
 // 定数定義
 const MAX_TITLE_LEN = 128;
 const MAX_BODY_LEN = 512;
+// 記事判定用のキーワード集(メンテナンス必要)
+const KEYWORDS = [
+    "アニメ","漫画","まんが",
+    "anime","新番組","監督",
+    "声優","せいゆう","アニメータ",
+    "作画","二次元","にじげん",
+    "プロデューサー","bd-box",
+    "ブルーレイ","blue-ray","box",
+    "blue","blueray","dvd",
+    "発売記念"
+];
+// NGワード
+const NG_KEYWORDS = [
+    "エロ","18禁","sex","アダルト"
+];
 
-/*
-loft_asagaya("http://www.loft-prj.co.jp/schedule/lofta").then(function(ret){
-    //console.log(ret);
+loft("http://www.loft-prj.co.jp/schedule/lofta").then(function(ret){
+    console.log(ret);
   }
 );
-loft_asagaya("http://www.loft-prj.co.jp/schedule/plusone").then(function(ret){
+/*
+loft("http://www.loft-prj.co.jp/schedule/plusone").then(function(ret){
     console.log(ret);
   }
 );
@@ -60,7 +78,9 @@ function loft(url){
             // イベント名
             $('div[class="event clearfix program1"] h3').each(function(){
                 title = $(this).text();
-                title = title.replace(/\r?\n/g,"");
+                title = title.replace(/\r?\n/g,"").toLowerCase();
+                // 文字列の正規化
+                title = moji(title).convert('ZE', 'HE').convert('ZS', 'HS').convert('HK', 'ZK').toString();
                 if(title.length >= MAX_TITLE_LEN-3){
                     title.substr(0,MAX_TITLE_LEN) + '...'
                 }
@@ -69,19 +89,42 @@ function loft(url){
             // イベント詳細
             $('p[class="month_content"]').each(function(){
                 body = $(this).text();
-                body = body.replace(/\r?\n/g,"");
+                body = body.replace(/\r?\n/g,"").toLowerCase();
+                // 文字列の正規化
+                body = moji(body).convert('ZE', 'HE').convert('ZS', 'HS').convert('HK', 'ZK').toString();
                 if(body.length >= MAX_BODY_LEN){
                     body.substr(0,MAX_BODY_LEN) + '...'
                 }
                 bodies.push(body);
             })
+
+            //　event情報フィルタ
             for(var i = 0; i < daies.length; i++){
-                // 記載のイベントが現在時刻よりも過去の場合保存配列に入れない。
-                var event_utime = new Date(daies[i]).getTime();           
+                // 1) 開催日フィルタ(取得時よりも古いeventは表示しない)
+               var event_utime = new Date(daies[i]).getTime();           
                 if(event_utime < utime){
                     continue;
                 }
-                ret.push([daies[i], titles[i], bodies[i]]);
+                // 2) ジャンル判定(アニメ関連か？)
+                for(var j = 0; j < KEYWORDS.length; j++){
+                    kw = KEYWORDS[j];
+                    // 正規表現でマッチングする(遅いけどまーこのレコード数ならいいだろ)
+                    var regexp = new RegExp(kw);
+                    if(titles[i].match(regexp) || bodies[i].match(regexp)){
+                        // 適合イベントに対してNG_KEYWORDで最後に確認する
+                        for(var k = 0; k < NG_KEYWORDS.length; k++){
+                            ng_kw = NG_KEYWORDS[k];
+                            var ng_regexp = new RegExp(ng_kw);
+                            if (titles[i].match(ng_regexp) || bodies[i].match(ng_regexp)){
+                                // NGに適合したらイベントとして採用しない
+                                break;
+                            }
+                        }
+                        // イベント名,イベント詳細のどちらかに指定してキーワードが存在したらKWループをbreakする
+                        ret.push([daies[i], titles[i], bodies[i]]);
+                        break;
+                    }
+                }
             }
             resolve(ret);
         })
