221194b53027a7bd88caf1d0a3511743e193bad4
Hisashi Itoh <it.hisas@gmail.com>
2018-07-01 11:22:29 +0900

    作成中

--- node/skill/scraping.js
+++ node/skill/scraping.js
@@ -18,9 +18,12 @@ client.set('browser','chrome');
 const MAX_TITLE_LEN = 128;
 const MAX_BODY_LEN = 512;
 
-loft_asagaya("http://www.loft-prj.co.jp/schedule/lofta")
+loft_asagaya("http://www.loft-prj.co.jp/schedule/lofta").then(function(ret){
+    console.log(ret);
+  });
 
-function fetch(){
+async function trim(text){
+    return text.replace(/\r?\n/g,"");
 }
 
 // URL毎の個別のスクレイピン関数
@@ -28,29 +31,38 @@ function fetch(){
 // TOP:   http://www.loft-prj.co.jp/lofta/
 // EVENT: http://www.loft-prj.co.jp/schedule/lofta
 function loft_asagaya(url){
-    var ary = [];
-    client.fetch(url, {}, function(err, $, res, body){
-        var day = undefined;
-        var title = undefined;
-        var body = undefined;
-        // 表構造になっている
-        // [日付,イベント名,イベント詳細] を配列に詰める
-        // 開催の日付
-        $('th[class=day]').each(function(){
-            day = $(this).text();
-        }),
-        // イベント名
-        $('div[class="event clearfix program1"] h3').each(function(){
-            title = $(this).text();
-        }),
-        // イベント詳細
-        $('p[class="month_content"] h3').each(function(){
-            body = $(this).text();
-        }),
-        console.log(day);
-        ary.push([day, title, body]);
+    return new Promise(function(resolve){
+        var daies = [];
+        var titles = [];
+        var bodies = [];
+        var ret = [];
+        client.fetch(url, {}, function(err, $, res, body){
+            // 表構造になっている
+            // [日付,イベント名,イベント詳細] を配列に詰める
+            // 開催の日付
+            $('th[class=day]').each(function(){
+                day = $(this).text();
+                daies.push(day);
+            })
+            // イベント名
+            $('div[class="event clearfix program1"] h3').each(function(){
+                title = $(this).text();
+                title = trim(title).then();
+                titles.push(title);
+            })
+            // イベント詳細
+            $('p[class="month_content"]').each(function(){
+                body = $(this).text();
+                body = trim(body).then();
+                bodies.push(body);
+            })
+            for(var i = 0; i < daies.length; i++){
+                ret.push([daies[i], titles[i], bodies[i]]);
+            }
+            resolve(ret);
+        })
     });
-    console.log(ary);
-}
+} 
+
 function loft_plusone(){
 }
